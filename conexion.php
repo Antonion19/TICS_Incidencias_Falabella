4cf340b09070d3c428d7798f7a240c4fcab55ced
Antonion19 <marco.leon07@hotmail.com>
2025-11-13 23:54:44 -0500

    BD hosteada

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root"; // usuario por defecto de XAMPP
-$pass = "";     // por defecto sin contraseña
-$db   = "falabella_incidencias";
+$host = "db.fr-pari1.bengt.wasmernet.com";   // Host del servicio
+$port = 10272;                                // Puerto proporcionado
+$user = "6b56ac8e7d0580002e21c4d0f7d7";       // Usuario
+$pass = "06916b56-ac8e-7e7f-8000-291d38db31bc"; // Password
+$db   = "dbPWUkaUgT7utrYpRj8h25dm";            // Nombre de la BD
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Error en la conexión: " . $conn->connect_error);
